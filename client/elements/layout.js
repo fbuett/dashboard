d00a4261724f15095349f775b7ec887ba523017e
fbuett <frank.buettner@gmail.com>
2016-04-23 23:20:27 +0200

    fixed 'collapse on link click' issue

--- client/elements/layout.js
+++ client/elements/layout.js
@@ -1,3 +1,11 @@
+Template.home.events({
+	'click .navbar-collapse.in': function (e) {
+	    if( $(e.target).is('a') ) {
+	        $('.navbar-collapse').collapse('hide');
+    	}
+	}
+})
+
 Template.home.helpers ({
 	/*
 	  get current Meteor release version
